08c9dd84a287d493ac2cf0a8f1820982cb4739f6
Sergey <kolmakov.sv@gmail.com>
2024-01-20 22:45:36 +0300

    Добавлена регистрация пользователей

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,6 +1,20 @@
 from django import forms
-
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, label="Password")
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Repeat Password")
+
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "email")
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd["password"] != cd["password2"]:
+            raise forms.ValidationError("Пароли не совпадают.")
+        return cd["password2"]
